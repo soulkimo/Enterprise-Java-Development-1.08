87e41daab65352419b2baca470861151b4a369a0
Kemberlyn MG <kemberlynmg@gmail.com>
2022-09-04 20:05:33 +0200

    update add

--- Enterprise-Java-Development-1.08/src/main/java/com/ironhack/Main.java
+++ Enterprise-Java-Development-1.08/src/main/java/com/ironhack/Main.java
@@ -1,24 +1,44 @@
 package com.ironhack;
 
+import com.ironhack.bigdecimal.BigDecimalUsage;
 import com.ironhack.integerlist.IntArrayList;
 
-public class Main {
-    public static void main(String[] args) {
-        IntArrayList intArrayList = new IntArrayList();
-        System.out.println("Array created void:" + intArrayList.toString());
-        intArrayList.add(20);
-        intArrayList.add(30);
-        intArrayList.add(40);
-        intArrayList.add(50);
-        intArrayList.add(60);
+import java.math.BigDecimal;
+import java.sql.SQLOutput;
 
-        for(int i=0; i<10; i++) {
+public class Main {
+    private static IntArrayList intArrayList;
+    private static BigDecimalUsage bigDecimalUsage;
+    public static void main(String[] args) {
+        intArrayList = new IntArrayList();
+        System.out.println("Array created void:" + intArrayList.toString());
+        for(int i=0; i<20; i++) {
             intArrayList.add(i);
         }
 
-        System.out.println(intArrayList);
+        System.out.println("Array result:" + intArrayList.toString());
+
+        BigDecimal bigDecimalRound = new BigDecimal("4.2545");
+        BigDecimal bigDecimalPositive = new BigDecimal("1.2345");
+        BigDecimal bigDecimalNegative = new BigDecimal("-45.67");
+        BigDecimal bigDecimalZero = new BigDecimal("0");
+
+
+        bigDecimalUsage = new BigDecimalUsage();
+        System.out.println("\n BigDecimal number rounded to the nearest hundredth.");
+        System.out.println(bigDecimalUsage.rounds(bigDecimalRound));
+        System.out.println("\n BigDecimal number positive rounded to the nearest tenth and reverses the sign");
+        System.out.println(bigDecimalUsage.reverseBigDecimal(bigDecimalPositive));
+        System.out.println("\n BigDecimal number negative rounded to the nearest tenth and reverses the sign");
+        System.out.println(bigDecimalUsage.reverseBigDecimal(bigDecimalNegative));
+        System.out.println("\n BigDecimal number ZERO rounded to the nearest tenth and reverses the sign");
+        System.out.println(bigDecimalUsage.reverseBigDecimal(bigDecimalZero));
+
 
         }
+
+
+
     }
 
 
--- Enterprise-Java-Development-1.08/src/main/java/com/ironhack/bigdecimal/BigDecimalUsage.java
+++ Enterprise-Java-Development-1.08/src/main/java/com/ironhack/bigdecimal/BigDecimalUsage.java
@@ -1,14 +1,27 @@
 package com.ironhack.bigdecimal;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 
 public class BigDecimalUsage {
 
-    public BigDecimal reverse(BigDecimal bigDecimal){
-        return bigDecimal;
+    public double reverseBigDecimal(BigDecimal bigDecimal){
+        BigDecimal zero = BigDecimal.ZERO;
+        if(bigDecimal.compareTo(zero) == 1){ // is greater
+            bigDecimal = bigDecimal.negate();
+        } else if(bigDecimal.compareTo(zero) == -1) { //is less
+            bigDecimal = bigDecimal.negate();
+        }else{
+            //The value is zero
+            System.out.println("The value is zero");
+        }
+        return bigDecimal.setScale(1, RoundingMode.HALF_DOWN).doubleValue();
     }
 
-    public  BigDecimal rounds(BigDecimal bigDecimal){
-        return bigDecimal;
+    public  double rounds(BigDecimal bigDecimal){
+
+        return bigDecimal.setScale(2, RoundingMode.HALF_UP).doubleValue();
     }
+
 }
